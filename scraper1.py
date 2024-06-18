521b79d118ce503455a8f56c3753cf4dbf5f7ca9
強哥 <57713874+zionjac@users.noreply.github.com>
2024-06-18 22:30:42 +0800

    0618-HW01

--- scraper1.py
+++ scraper1.py
@@ -1,1 +1,15 @@
-print('Hello Git!')
+import requests
+import time
+from bs4 import BeautifulSoup
+url = 'https://www.bbc.com/zhongwen/trad/topics/c32p4kj2yzqt'
+
+for i in range(1,4):
+    print(f"第 {i} 頁")
+    r = requests.get(url)
+    sp = BeautifulSoup(r.text, 'html.parser')
+    datas=sp.select('h2')
+    for j,bbc in enumerate(datas):
+      print(f'{j+1} {bbc.text}' ,end='\t')
+      print(bbc.a.get('href'))
+    url = 'https://www.bbc.com/zhongwen/trad/topics/c32p4kj2yzqt'+'?page='+str(i+1)
+    time.sleep(3)
